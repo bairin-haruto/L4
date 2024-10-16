45edd6dc222bce468067b3238d0f53237db460cc
梅林遥大 <2211140011r@ed.fuk.kindai.ac.jp>
2024-10-16 05:37:11 +0000

    pass

---
+++ pass.rb
@@ -0,0 +1,12 @@
+require 'bcrypt'
+
+signup_password = BCrypt::Password.create("my password")
+puts signup_password
+
+if User.find_by(uid: params[:uid])
+    login_password = BCrypt::Password.new(signup_password)
+    if login_password == 'my password'
+        p "ログイン成功"
+    end
+end
+
